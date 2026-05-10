9ce0124622a003b702fa784b9afdb0ba3a8227ba
23Ihirwe <h.ihirwe@alustudent.com>
2026-05-10 03:58:07 -0700

    Fix syntax error in integer_validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""
-BaseGeometry module
-"""
-
-
 class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        # must be an integer (bool is NOT accepted)
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
-
-        # must be greater than 0
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
